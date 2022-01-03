fbcd8e0604b42b14c7fd425156af1b1894dd75cd
wmx11 <vmykolaitis1@gmail.com>
2022-01-03 19:58:18 +0200

    Holders command will now cache its content for 5 minutes

--- commands/holders.js
+++ commands/holders.js
@@ -1,8 +1,29 @@
 const getHolders = require('../modules/scrapers/bscscan/getHolders');
+const cacheState = require('../utils/cacheState');
+const setCacheState = require('../utils/setCacheState');
+
+const state = cacheState;
 
 module.exports = async (message) => {
   const { channel } = message;
-  const holders = await getHolders();
-  const holdersMessage = `Titano currently has **${parseInt(holders, 10).toLocaleString('en-US')}** holders`;
-  return channel.send(holdersMessage);
+  const fetchingMessage = await channel.send('Fetching holders...');
+
+  try {
+    if (state.isFetched) {
+      fetchingMessage.delete();
+      return channel.send(state.cachedData);
+    }
+
+    const holders = await getHolders();
+    const holdersMessage = `Titano currently has **${parseInt(
+      holders,
+      10
+    ).toLocaleString('en-US')}** holders`;
+
+    fetchingMessage.delete();
+    setCacheState(state, holdersMessage);
+    return channel.send(holdersMessage);
+  } catch (error) {
+    return channel.send("Bzzzzt. Couldn't fetch holders :( Try again a little later.");
+  }
 };
