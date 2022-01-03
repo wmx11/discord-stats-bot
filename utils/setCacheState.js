57ff098c0322e4f3a29b70dcc7e7fc7c9b19a162
wmx11 <vmykolaitis1@gmail.com>
2022-01-03 19:59:31 +0200

    Added cache state setter

---
+++ utils/setCacheState.js
@@ -0,0 +1,15 @@
+const { dataFetchCacheTimeout } = require('../config');
+
+module.exports = (state, dataToCache) => {
+  if (!state) {
+    return;
+  }
+
+  state.isFetched = true;
+  state.cachedData = dataToCache;
+
+  setTimeout(() => {
+    state.isFetched = false;
+    state.cachedData = null;
+  }, dataFetchCacheTimeout);
+};
