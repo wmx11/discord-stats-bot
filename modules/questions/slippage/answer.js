b5a86131509d97a6982da1f56ef2d31f5dda3a8c
wmx11 <vmykolaitis1@gmail.com>
2022-01-04 22:43:17 +0200

    added slippage related questions and answers

---
+++ modules/questions/slippage/answer.js
@@ -0,0 +1,9 @@
+module.exports = `
+If you are having a hard time buying or selling, let\'s take a look. To start, Titano requires a tax of **13%** to buy and **18%** to sell.
+
+Now, ensure that slippage is set to **13% for a sell order** and **18% for a buy order** as a base on Pancakeswap and gradually increase by **1%** until the transaction succeeds.
+
+You can also use PooCoin to set auto-slippage if this still doesn\'t work.
+
+You can find the links in the <#913849034094051399> channel.
+`;
