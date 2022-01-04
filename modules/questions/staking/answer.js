686dcd61db0fd5c92e5d3434cd693900707d1451
wmx11 <vmykolaitis1@gmail.com>
2022-01-04 22:43:26 +0200

    added staking related questions and answers

---
+++ modules/questions/staking/answer.js
@@ -0,0 +1,5 @@
+module.exports = `
+There is no “staking” in Titano so to speak. You do not need to connect your wallet or do anything special. The second you buy Titano, your account will automatically gain compounding interest every 30 minutes. 
+
+You can read more about this in the white paper found here: https://docs.titano.finance
+`;
