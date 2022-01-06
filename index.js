4295df94dad56fe92c809836fd53924999a4f76b
wmx11 <vmykolaitis1@gmail.com>
2022-01-06 17:20:05 +0200

    Index.js tweaks. Added interaction listeners. Added express listener.

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 require('dotenv').config();
+
 const { Client, Intents } = require('discord.js');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+const handleInteractionCreate = require('./utils/interactions/handleInteractionCreate');
+const handleMenuInteraction = require('./utils/interactions/handleMenuInteraction');
 const runCommands = require('./utils/runCommands');
+
+const isInAllowedChannel = require('./utils/isInAllowedChannel');
+const sendNotAllowedChannelMessage = require('./utils/sendNotAllowedChannelMessage');
+
+// Load DB models
+require('./models/Stats');
+
+// Load Routets
+require('./routes/databaseRoutes')(app);
+
+// Connect to the DB
+try {
+  mongoose.connect(process.env.MONGO_URI);
+} catch (error) {
+  console.log(error);
+}
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -14,11 +37,47 @@ client.once('ready', () => {
   });
 });
 
+/**
+ * @desc - Used for slash commands interaction
+ */
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) {
+    return;
+  }
+  handleInteractionCreate(interaction);
+});
+
+/**
+ * @desc - Used for menu selections
+ * From slash commands
+ */
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isSelectMenu()) {
+    return;
+  }
+
+  handleMenuInteraction(interaction);
+});
+
+/**
+ * @desc - Read every message.
+ * Also used for !! commands
+ */
 client.on('messageCreate', async (message) => {
   if (message.author.bot) {
+    if (
+      !isInAllowedChannel(message) &&
+      message.interaction?.commandName === 'charts'
+    ) {
+      sendNotAllowedChannelMessage(message);
+    }
+
     return;
   }
   runCommands(message);
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+// Listen to incoming requests
+app.listen(process.env.PORT);
