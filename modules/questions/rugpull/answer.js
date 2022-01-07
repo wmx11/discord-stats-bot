ef8e82f37e18e6645086152134d36dbc2c8a9747
wmx11 <vmykolaitis1@gmail.com>
2022-01-07 10:54:03 +0200

    Added rugpull related questions

---
+++ modules/questions/rugpull/answer.js
@@ -0,0 +1,10 @@
+module.exports = `
+Titano Bot senses your uncertainty.
+
+First of all, take some time and familiarize yourself with the project. 
+You will find the official docs here: https://docs.titano.finance/ 
+
+If you have any further questions - feel free to ask our community! They rock!
+Be advised: Invest at your own risk.
+Never invest more than you can afford to lose.
+`;
---
+++ modules/questions/rugpull/keywords.js
@@ -0,0 +1,13 @@
+module.exports = [
+  'is this a rug',
+  'is this rug',
+  'is titano rug',
+  'is titano a rug',
+  'is titano legit',
+  'is this project legit',
+  'when will titano rug',
+  'how can I be sure titano is not a rug',
+  'how can I be sure titano is not rug',
+  "how can I be sure titano is not won't rug",
+  'how can I be sure titano is not will not rug',
+];
