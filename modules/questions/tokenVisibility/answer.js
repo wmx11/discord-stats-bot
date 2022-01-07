8bc744f2629ca493d6e761f7a17028b073ffc7b6
wmx11 <vmykolaitis1@gmail.com>
2022-01-07 10:54:51 +0200

    Added token visibility related questions

---
+++ modules/questions/tokenVisibility/answer.js
@@ -0,0 +1,7 @@
+module.exports = `
+If you can’t see the token in your wallet, it may be an easy fix. Firstly it depends on what wallet you have. If you have MetaMask, you will need to “Import Token” by adding the contract address. Also make sure your account is set to the Binance Smart Chain.
+
+For Trust wallet, you can tap “Send” or “Receive” then type in Titano. When shown the message the token cannot be found, hit add token. Select BSC network and then paste the Contract Address in. 
+
+For reference, the Contract Address is: \`0xBA96731324dE188ebC1eD87ca74544dDEbC07D7f\`
+`;
