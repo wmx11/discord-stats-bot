c79a2aa12cf373dfa99d3427e6fc7b20bc9b4b40
wmx11 <vmykolaitis1@gmail.com>
2022-01-11 23:07:43 +0200

    Added bolding

--- commands/rfv.js
+++ commands/rfv.js
@@ -3,5 +3,5 @@ const getRfv = require('../modules/statsGetters/getRfv');
 module.exports = async (message) => {
   const { channel } = message;
   const rfv = await getRfv();
-  return channel.send(`TITANO RFV Balance: ${rfv}`);
+  return channel.send(`**TITANO RFV Balance**: ${rfv}`);
 };
--- commands/treasury.js
+++ commands/treasury.js
@@ -3,5 +3,5 @@ const getTreasury = require('../modules/statsGetters/getTreasury');
 module.exports = async (message) => {
   const { channel } = message;
   const treasury = await getTreasury();
-  return channel.send(`TITANO Treasury Balance: ${treasury}`);
+  return channel.send(`**TITANO Treasury Balance**: ${treasury}`);
 };
