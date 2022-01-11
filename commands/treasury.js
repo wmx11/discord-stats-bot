ba5ee08cc6720812f9de8795211bc4f69fbeba71
wmx11 <vmykolaitis1@gmail.com>
2022-01-11 23:05:46 +0200

    Added treasury command

---
+++ commands/treasury.js
@@ -0,0 +1,7 @@
+const getTreasury = require('../modules/statsGetters/getTreasury');
+
+module.exports = async (message) => {
+  const { channel } = message;
+  const treasury = await getTreasury();
+  return channel.send(`TITANO Treasury Balance: ${treasury}`);
+};
