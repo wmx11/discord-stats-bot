57dd8874ee04ec28f5d765008b3dc8d3cf42fc7f
wmx11 <vmykolaitis1@gmail.com>
2022-01-11 23:05:40 +0200

    Added rfv command

---
+++ commands/rfv.js
@@ -0,0 +1,7 @@
+const getRfv = require('../modules/statsGetters/getRfv');
+
+module.exports = async (message) => {
+  const { channel } = message;
+  const rfv = await getRfv();
+  return channel.send(`TITANO RFV Balance: ${rfv}`);
+};
