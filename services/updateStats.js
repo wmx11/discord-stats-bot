c3b0ee4f6e2217a10782693351671c72e1e3af80
wmx11 <vmykolaitis1@gmail.com>
2022-01-16 16:47:05 +0200

    Removed coingecko api from updateStats. Added titano and bnb price getters. Holders achieved message is now dynamic.

--- services/updateStats.js
+++ services/updateStats.js
@@ -1,4 +1,3 @@
-const CoinGecko = require('coingecko-api');
 const { format } = require('date-fns');
 
 const mongoose = require('mongoose');
@@ -14,52 +13,49 @@ const getMarketCap = require('../modules/dataGetters/bscscan/getMarketCap');
 const getRfvBalance = require('../modules/dataGetters/bscscan/getRfvBalance');
 const getTreasuryBalance = require('../modules/dataGetters/bscscan/getTreasuryBalance');
 
-/**
- * @desc - Init the coinGecko client
- */
-const CoinGeckoClient = new CoinGecko();
+const getBnbUsdtPrice = require('../modules/dataGetters/bnb/getBnbUsdtPrice');
+const getTitanoUsdPrice = require('../modules/dataGetters/titano/getTitanoUsdPrice');
 
-let isMessageSent = false;
+const milestones = {
+  nextMilestone: 0,
+  isSet: false,
+};
 
 module.exports = async () => {
   try {
-    /**
-     * @desc - Titano coin
-     */
-    const {
-      data: { market_data: titano },
-    } = await CoinGeckoClient.coins.fetch('titano', {});
-
-    /**
-     * @desc - BNB coin
-     */
-    const {
-      data: { market_data: bnb },
-    } = await CoinGeckoClient.coins.fetch('binancecoin', {});
-
     const holders = await getHolders();
-    const marketCap = await getMarketCap(titano.current_price.usd);
-    const treasuryBalance = await getTreasuryBalance(bnb.current_price.usd);
-    const rvfBalance = await getRfvBalance(bnb.current_price.usd);
+    const bnbUsdtPrice = await getBnbUsdtPrice();
+    const titanoUsdPrice = await getTitanoUsdPrice(bnbUsdtPrice);
+    const marketCap = await getMarketCap(titanoUsdPrice);
+    const treasuryBalance = await getTreasuryBalance(bnbUsdtPrice);
+    const rvfBalance = await getRfvBalance(bnbUsdtPrice);
+
+    const getNextMilestone = () => Math.floor(holders / 1000) * 1000 + 1000;
 
     await new Stats({
       marketCap: marketCap,
       rfv: rvfBalance,
       treasury: treasuryBalance,
-      price: titano.current_price.usd,
+      price: titanoUsdPrice,
       holders: holders,
       date: Date.now(),
     }).save();
+
+    if (!milestones.isSet) {
+      milestones.nextMilestone = getNextMilestone();
+      milestones.isSet = true;
+    }
 
     eventBus.emit(stats.update);
 
     console.log(`${format(Date.now(), 'yyy-MM-dd HH:mm:ss')} - Stats Updated`);
 
-    if (!isMessageSent && parseInt(holders, 10) >= 24000) {
-      await axios('https://service-7957.something.gg/bot-hype/908073303732813880/TITANO%20has%20just%20reached%2023k%20holders%21');
-      isMessageSent = true;
+    if (holders >= milestones.nextMilestone) {
+      milestones.nextMilestone = getNextMilestone();
+      const holdersAchievedMessage = encodeURI(`**TITANO has just reached ${Math.floor(holders / 1000)}k holders!** 🚀🚀🚀`);
+      await axios(`https://service-7957.something.gg/bot-hype/908073303732813880/${holdersAchievedMessage}`)
     }
-    
+
   } catch (error) {
     console.log(error);
   }
