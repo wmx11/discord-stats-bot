43019c0cc8d99a83330d156ac808d0bbc352dcb7
wmx11 <vmykolaitis1@gmail.com>
2022-01-16 16:45:33 +0200

    Tweaked circulating supply percentage

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   treasuryAddress: '0x4DD90D3cE962039A3c66d613207aC2d449dFa04F',
   rfvAddress: '0x00dE99c90E8971D3E1c9cBA724381B537F6e88C1',
   startDate: '2021-11-17 16:34:35',
-  circulatingSupply: 0.03,
+  circulatingSupply: 0.026146,
   decimalPlaces: 10 ** 18,
   rewards: {
     perDay: 0.019174073,
