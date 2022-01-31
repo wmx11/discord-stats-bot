b71aa364aac4748d497a76591188172a0c80a0c7
wmx11 <vmykolaitis1@gmail.com>
2022-01-31 12:06:35 +0200

    AMA keywords and answers added

---
+++ modules/questions/ama/answer.js
@@ -0,0 +1,5 @@
+module.exports = `
+**TITANO AMA January 21, 2022**
+You can find the latest Titano AMA on the official Titano Finance YouTube channel:
+<https://www.youtube.com/watch?v=1GajL7Hj1RY>
+`;
