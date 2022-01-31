ca4bd5ebec5e68cc7352ffb21292e20322165480
wmx11 <vmykolaitis1@gmail.com>
2022-01-31 12:01:40 +0200

    Added more data to the config file

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 module.exports = {
   clientId: '927475904735768596',
   guildId: '918902350150787102',
+  mainChannelId: '918902350150787105',
   contractAddress: '0xBA96731324dE188ebC1eD87ca74544dDEbC07D7f',
   treasuryAddress: '0x4DD90D3cE962039A3c66d613207aC2d449dFa04F',
   rfvAddress: '0x00dE99c90E8971D3E1c9cBA724381B537F6e88C1',
+  playAddress: '0x3ac632f97f389290c61289ecc03564c2a4c20882',
   startDate: '2021-11-17 16:34:35',
+  playDrawHour: '14:00:00',
+  playDurationDays: 3,
+  botEndpoint: 'http://localhost:2000',
   circulatingSupply: 0.026146,
   decimalPlaces: 10 ** 18,
   rewards: {
@@ -29,6 +34,7 @@ module.exports = {
     treasury: '930546026572550144',
     price: '931270784289214484',
     holders: '931270953470689310',
+    marketcap: '933741907119579137'
   },
   blacklist: {
     users: ['897266614796447754'],
@@ -49,6 +55,7 @@ module.exports = {
     },
     { name: 'treasury', description: 'Shows $TITANO Treasury value' },
     { name: 'rfv', description: 'Shows $TITANO RFV value' },
+    { name: 'play', description: 'Shows TITANO PLAY stats' },
     { name: 'kiss', description: 'The bot just kisses you. Thank you.' },
     { name: 'help', description: 'Shows all available commands' },
   ],
