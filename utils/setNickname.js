c6634ce00d82e822cbf416fab63dcf9cae405a69
wmx11 <vmykolaitis1@gmail.com>
2022-02-09 08:54:45 +0200

    Updated setNickname method. Now it updates on multiple guilds.

--- utils/setNickname.js
+++ utils/setNickname.js
@@ -1,7 +1,12 @@
 const { guildId } = require('../config');
 
-module.exports = (client, botId, nickname) =>
-  client.guilds.cache
-    .get(guildId)
+module.exports = (client, botId, nickname) => {
+  client.guilds.cache.forEach((guild) => {
+    const { id } = guild;
+
+    client.guilds.cache
+    .get(id)
     .members.cache.get(botId)
     .setNickname(nickname);
+  });
+};
