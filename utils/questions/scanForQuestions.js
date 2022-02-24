2596c13edccd86ebf5779e76e7843c1e6b59dc3b
Vainius <vmykolaitis1@gmail.com>
2022-02-24 11:04:23 +0200

    Added allowedChannels checker

--- utils/questions/scanForQuestions.js
+++ utils/questions/scanForQuestions.js
@@ -2,12 +2,11 @@ const sanitizeString = require('../sanitizeString');
 const questionsAndAnswers = require('./questionsAndAnswers');
 
 module.exports = (message) => {
-  const { content } = message;
+  const { content, channelId } = message;
 
   const sanitizedContent = sanitizeString(content);
 
   const foundAnswer = questionsAndAnswers.reduce((answer, question) => {
-
     const foundQuestion = question.keywords.find((phrase) => {
       return sanitizedContent.includes(phrase);
     });
@@ -20,6 +19,10 @@ module.exports = (message) => {
   }, {});
 
   if (!Object.keys(foundAnswer).length) {
+    return;
+  }
+
+  if (foundAnswer.channels.find((channel) => channel === channelId)) {
     return;
   }
 
