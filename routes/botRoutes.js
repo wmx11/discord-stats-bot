b1d11d850aa9fd82c26603ae66fef6657e9df0c3
Vainius <vmykolaitis1@gmail.com>
2022-02-24 11:03:37 +0200

    Added additional bot routes

--- routes/botRoutes.js
+++ routes/botRoutes.js
@@ -1,3 +1,6 @@
+const axios = require('axios');
+const { botEndpoint, mainChannelId } = require('../config');
+
 module.exports = (app) => (client) => {
   app.get('/bot-say/:channelId/:message', (req, res) => {
     res.send('Bot has spoken');
@@ -20,5 +23,54 @@ module.exports = (app) => (client) => {
         console.log(error);
       }
     }
+  });
+
+  app.get('/bot-scam-disclaimer', (req, res) => {
+    const message = `
+    **REMINDER**
+    Titano has not and never will make a group chat to **"airdrop"**. 
+    Any tokens sent to your wallet without request are a scam.
+    Any DMs from ANYONE claiming to be a mod or support is fake.
+
+    Titano does not have "Official airdrop" groups.
+    Titano does not have "Official claim your rewards" groups.
+    `;
+  
+    res.send('Disclaimer');
+
+    axios({
+      url: `${botEndpoint}/bot-post`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        channelId: mainChannelId,
+        message,
+      },
+    })
+  });
+
+  app.get('/bot-offtopic-disclaimer', (req, res) => {
+    const message = `
+**World news have affected all markets including Titano.**
+The Ukraine and Russia political conflict and special military operations have affected the global markets, including cryptocurrencies.
+We understand your concerns and we want you to move political and geopolitical conflict talk to <#908073303942508664>
+Thank you.
+    `;
+  
+    res.send('Off topic disclaimer');
+
+    axios({
+      url: `${botEndpoint}/bot-post`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        channelId: mainChannelId,
+        message,
+      },
+    })
   });
 };
